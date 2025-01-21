7f234ce6ad35e0c0afcf26dffedc2b6122584884
silverhyuk <tldn233@gmail.com>
2025-01-21 22:40:51 +0900

    first commit

--- src/main/java/com/david/grpcdemo/GrpcDemoClient.java
+++ src/main/java/com/david/grpcdemo/GrpcDemoClient.java
@@ -1,5 +1,32 @@
-package com.david.client;
+package com.david.grpcdemo;
 
-public class GrpcClient {
+import com.david.grpcdemo.Hello.HelloRequest;
+import com.david.grpcdemo.Hello.HelloResponse;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
 
+public class GrpcDemoClient {
+    public static void main(String[] args) {
+        // 서버 주소와 포트
+        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 50051)
+            .usePlaintext()
+            .build();
+
+        // gRPC 클라이언트 생성
+        HelloServiceGrpc.HelloServiceBlockingStub blockingStub = HelloServiceGrpc.newBlockingStub(channel);
+
+        // 요청 객체 생성
+        HelloRequest request = HelloRequest.newBuilder()
+            .setName("World")
+            .build();
+
+        // 서버 호출
+        HelloResponse response = blockingStub.sayHello(request);
+
+        // 응답 출력
+        System.out.println("Received: " + response.getMessage());
+
+        // 채널 종료
+        channel.shutdown();
+    }
 }
--- src/main/java/com/david/grpcdemo/service/HelloServiceImpl.java
+++ src/main/java/com/david/grpcdemo/service/HelloServiceImpl.java
@@ -1,4 +1,21 @@
 package com.david.grpcdemo.service;
 
-public class HelloServiceImpl {
+import com.david.grpcdemo.Hello.HelloRequest;
+import com.david.grpcdemo.Hello.HelloResponse;
+import com.david.grpcdemo.HelloServiceGrpc;
+import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
+
+@GrpcService
+public class HelloServiceImpl extends HelloServiceGrpc.HelloServiceImplBase {
+    @Override
+    public void sayHello(HelloRequest request, StreamObserver<HelloResponse> responseObserver) {
+        System.out.println("Request received from client:\n" + request);
+
+        String greeting = "Hello, " + request.getName() + "!";
+        HelloResponse response = HelloResponse.newBuilder().setMessage(greeting).build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
 }
